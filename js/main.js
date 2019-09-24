7ee576b904764b4214e30b97615767d2e2b35a22
Ejex Xeffer <ejexefer@gmail.com>
2019-09-24 21:28:30 +0500

    work on menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// function on() {
+//     document.getElementById("menu").style.display = "flex";
+//   }
+  
+//   function off() {
+//     document.getElementById("menu").style.display = "none";
+//   }
+
+var mobileMenu = document.getElementById("listMenu");
+var threeBarsTopNav = document.getElementById("topNavMenu");
+threeBarsTopNav.onclick = mobileMenu.style.display = "flex";
