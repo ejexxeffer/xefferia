fd9a014cf8df03a43ae276d702d0d74c617603de
Ejex Xeffer <ejexefer@gmail.com>
2019-11-26 03:26:05 +0500

    write menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
-// function on() {
-//     document.getElementById("menu").style.display = "flex";
-//   }
-  
-//   function off() {
-//     document.getElementById("menu").style.display = "none";
-//   }
+"use strict";
+//почему то не работает с первого раза
+// function toggle() {
+//     if (mobileMenu.style.display==="block") {
+//         mobileMenu.style.display = "none";
+//     } else {
+//         mobileMenu.style.display = "none";
+//     }
+// }
 
-var mobileMenu = document.getElementById("listMenu");
-var threeBarsTopNav = document.getElementById("topNavMenu");
-threeBarsTopNav.onclick = mobileMenu.style.display = "flex";
+let mobileMenu = document.getElementById("listmenu").style;
+let threeBars = document.getElementById("threebars");
+let threeBars_close = document.getElementById("threebars_close");
+let scroll_lock = document.body.style;
+
+
+// function off() {
+//     return mobileMenu.style.display = "none";
+// }
+
+// function on() {
+//     return mobileMenu.style.display = "block";
+// }
+
+//overflow здесь <div style="overflow">
+function toggle() {
+    if (mobileMenu.display === "block") {
+        scroll_lock.overflow = null; 
+        mobileMenu.display = null; 
+    } else {
+        scroll_lock.overflow = "hidden";
+        mobileMenu.display = "block";
+    }
+}
+
+// threeBars.onclick = on;
+// threeBars_close.onclick = off;
+if (window.matchMedia("(max-width: 700px)").matches) {
+    threeBars.onclick = toggle;
+    threeBars_close.onclick = toggle;
+  }
