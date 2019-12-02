d8eb3c47ac458379f619c896d76de6a8660f2e89
Ejex Xeffer <ejexefer@gmail.com>
2019-12-03 04:38:10 +0500

    Added markup for the search bar and removed trash from files and also fixed all the indents between searchbar menu and the searchbar

--- js/main.js
+++ js/main.js
@@ -1,26 +1,9 @@
 "use strict";
-//почему то не работает с первого раза
-// function toggle() {
-//     if (mobileMenu.style.display==="block") {
-//         mobileMenu.style.display = "none";
-//     } else {
-//         mobileMenu.style.display = "none";
-//     }
-// }
 
 let mobileMenu = document.getElementById("listmenu").style;
 let threeBars = document.getElementById("threebars");
 let threeBars_close = document.getElementById("threebars_close");
 let scroll_lock = document.body.style;
-
-
-// function off() {
-//     return mobileMenu.style.display = "none";
-// }
-
-// function on() {
-//     return mobileMenu.style.display = "block";
-// }
 
 //overflow здесь <div style="overflow">
 function toggle() {
@@ -33,9 +16,7 @@ function toggle() {
     }
 }
 
-// threeBars.onclick = on;
-// threeBars_close.onclick = off;
 if (window.matchMedia("(max-width: 700px)").matches) {
     threeBars.onclick = toggle;
     threeBars_close.onclick = toggle;
-  }
+}
