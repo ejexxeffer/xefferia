c08040722e5f859d9f48ea83663375f2058971ef
Ejex Xeffer <ejexefer@gmail.com>
2020-02-11 17:15:26 +0500

    first part of writing search on js

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,55 @@
-var gulp = require ('gulp');
-//const browserSync = require('browser-sync').create();
-//var reload = browserSync.reload;
-var browserSync = require('browser-sync');
+const gulp = require ('gulp');
+const connect = require ('gulp-connect-php');
+const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
-var config = {
-	server: {
-		baseDir: './',
-		index: 'index.html'
-	},
-	port: 3000,
-	// open: "local",	
-	notify: false
+const configSync = {
+  server: {
+    baseDir: './',
+    index: 'index.html'
+  },
+  port: 3000,
+  // open: "local",
+  notify: false
 };
 
+const configPhp = {
+  base: './',
+  keepalive: true,
+  hostname: 'localhost',
+  port: 8080,
+  open: false
+}
+
 gulp.task ('watch', function(done) {
-		gulp.watch('css/**/*.css').on('change', () => {
-			browserSync.reload();
-			done();
-		});
-		gulp.watch('*.html').on('change', () => {
-			browserSync.reload();
-			done();
-		});
-		gulp.watch('js/**/*.js').on('change', () => {
-			browserSync.reload();
-			done();
-		});
-	});
-
-
-gulp.task('serve', function(done){
-	browserSync(config);
-	done();
+  gulp.watch('css/**/*.css').on('change', () => {
+    reload();
+    done();
+  });
+  gulp.watch('*.html').on('change', () => {
+    reload();
+    done();
+  });
+  gulp.watch('js/**/*.js').on('change', () => {
+    reload();
+    done();
+  });
+  gulp.watch('*.php').on('change', ()=> {
+    reload();
+    done();
+  })
 });
 
-gulp.task('default', gulp.parallel('serve','watch'));
+gulp.task('serve', (done) => {
+  connect.server(configPhp, ()=> {
+    browserSync({proxy: '127.0.0.1:8080', notify: false});
+  });
+  done();
+});
+
+gulp.task('php', (done) => {
+ connect.server(configPhp);
+ done();
+});
+
+gulp.task('default', gulp.parallel(['serve','watch']));
--- js/main.js
+++ js/main.js
@@ -1,64 +1,140 @@
 "use strict";
 
+let form = document.getElementById('form');
+let searchForm = document.getElementById('search_form');
 let mobileMenu = document.getElementById('listmenu');
 let threeBars = document.getElementById('threebars');
 let threeBars_close = document.getElementById('threebars_close');
+let archiveGrid = document.getElementById('archive');
 
-function isValid(value) {
-  if (value) {
-    return (value.length >= 3) && (value.length <= 20);
-  }
+
+document.addEventListener('DOMContentLoaded', loadValid);
+
+function loadValid() {
+  threeBars.addEventListener('click',toggle);
+  threeBars_close.addEventListener('click',toggle);
+
+  if (form !== null) {
+    let nameInpt = form.querySelector('#name');
+    let emailInpt = form.querySelector('#email');
+    let themeInpt = form.querySelector('#theme');
+    let messageText = form.querySelector('#textmessage');
+    let submitBtn = form.querySelector('#submit');
+
+    nameInpt.addEventListener('input', ()=>{
+      submitBtn.disabled = !isValid(nameInpt);
+    });
+    
+    emailInpt.addEventListener('input', ()=>{
+      submitBtn.disabled = !isValid(emailInpt);
+    });
+    
+    themeInpt.addEventListener('input', ()=>{
+      submitBtn.disabled = !isValid(themeInpt);
+    });
+    
+    messageText.addEventListener('input', ()=>{
+      submitBtn.disabled = !isValid(messageText);
+    });
+
+    form.addEventListener('submit', submitFormHandler);
+  };
+  
+  if (searhForm !== null) {
+    let searchInpt = form.querySelector('#search');
+    let submitSearchBtn = form.querySelector('#submit_search');
+    buildArchive();
+
+    searchInpt.addEventListener('inpt', ()=>{
+      submitSearchBtn.disabled = !isValid(searchInpt);
+    });
+
+    searchForm.addEventListener('submit', submitFormHandler);
+  };
+
 }
 
-function isQuestion(value) {
-  if (value) {
-    return (value.length >=20) && (value.length <=500);
+function isValid(param) {
+  if (param.value.length && (param.tagName === 'TEXTAREA')) {
+    return (param.value.length >=20) && (param.value.length <=500);
+  } else if (param.value.length){
+    return (param.value.length >= 3) && (param.value.length <= 20);
   }
 }
 
 //overflow здесь <div style="overflow">
 function toggle() {
-  document.body.classList.toggle('overflow_hidden');
-  mobileMenu.classList.toggle('display_block');
+  if (window.matchMedia("(max-width: 700px)").matches) {
+    document.body.classList.toggle('overflow_hidden');
+    mobileMenu.classList.toggle('display_block');
+  } else {
+    document.body.className = '';
+    mobileMenu.className = '';
+  }
 }
 
-if (window.matchMedia("(max-width: 700px)").matches) {
-  threeBars.addEventListener('click',toggle);
-  threeBars_close.addEventListener('click',toggle);
-} else {
-  document.body.className = '';
-  mobileMenu.className = '';
+function htmlToCard(href,headerValue,date,theme) {
+  return `<section>
+            <div class="archive_header">
+              <h1><a href="${href}">${headerValue}</a></h1>
+            </div>
+            <div class="archive_archlabel">
+              <time>${date}</time>
+              <p>${theme}</p>
+            </div>
+          </section>`;
 }
 
-let form = document.getElementById('form');
-let nameInpt = form.querySelector('#name');
-let emailInpt = form.querySelector('#email');
-let themeInpt = form.querySelector('#theme');
-let messageText = form.querySelector('#textmessage');
-let submitBtn = form.querySelector('#submit');
+function buildJSON() {
+  //запрос файла с сервера
+  promiseXHR('GET', '../posts/20_04_2019')
+  .then((response) => {
+    href = '/posts/20_04_2019';
+    headerValue = response.querySelector('h1').innerHTML;
+    date = response.querySelector('time').innerHTML;
+    theme = response.querySelector('p').innerHTML;
+  })
+}
 
+function buildArchive() {
+ 
+  //тут должна быть реализация разбора json в объекты и создание карточек пока есть объекты
+  //создание карточки
+  archiveGrid.insertAdjacentHTML("beforeend",htmlToCard(href,headerValue,date,theme));
+}
 
-nameInpt.addEventListener('input', ()=>{
-  submitBtn.disabled = !isValid(nameInpt.value);
-});
+function promiseXHR(method, url, body) {
+  return new Promise ((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== 4) {
+        if (xhr.status == 200 && method == 'GET') {
+          resolve(xhr.response);
+        } else if (xhr.status == 200 && method == 'POST') {
+          resolve(xhr.response);
+        }
 
-emailInpt.addEventListener('input', ()=>{
-  submitBtn.disabled = !isValid(emailInpt.value);
-});
-
-themeInpt.addEventListener('input', ()=>{
-  submitBtn.disabled = !isValid(themeInpt.value);
-});
-
-messageText.addEventListener('input', ()=>{
-  submitBtn.disabled = !isValid(messageText.value);
-});
-
-form.addEventListener('submit', submitFormHandler);
+        if (xhr.status == 404) {
+          reject(xhr.statusText);
+        } else if (xhr.status == 500) {
+          reject(xhr.statusText);
+        }
+      }
+    }
+    xhr.open(method, url, true);
+    if (method == 'POST') {
+      xhr.setRequestHeader('Content-type', 'application/json');
+      xhr.responseType = 'json';
+    } else {
+      xhr.responseType = 'document';
+    }
+    xhr.send(body);
+  })
+}
 
 function submitFormHandler(event) {
   event.preventDefault();
-
+  
   if (isValid(nameInpt.value) && isValid(emailInpt.value) && isValid(themeInpt.value) && isValid(messageText.value)) {
     let question = {
       name: nameInpt.value.trim(),
@@ -69,12 +145,7 @@ function submitFormHandler(event) {
     }
     submitBtn.disabled = true;
     //Async question to server to send mail
-    fetch('mail.php', {
-      method: 'POST',
-      body: JSON.stringify(question),
-      headers: {
-          'Content-type': 'application/json'}})
-    .then(response => response.json())
+    promiseXHR('POST','../mail.php', JSON.stringify(question))
     .then(response => {
 
       console.log('response from server', response);
@@ -91,16 +162,7 @@ function submitFormHandler(event) {
       themeInpt.value = '';
       messageText.value = '';
     })
+  } else if (isValid(searchInpt.value)) {
+    
   }
-}
-
-function addToLocalStorage(question) {
-  const questions = getQuestiondFromLocalStorage();
-  questions.push(question);
-  localStorage.setItem('questions', JSON.stringify(questions));
-}
-
-
-function getQuestiondFromLocalStorage() {
-  return JSON.parse(localStorage.getItem('questions')||'[]');
 }
